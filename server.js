d60f3b215ffa4e24bd5e7d7f24e7d49fc2bdf950
bergconnor <cberg1@ksu.edu>
2017-02-23 17:17:23 -0600

    added staticFiles.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 /* global variables */
 var multipart = require('./multipart');
 var template = require('./template');
+var staticFiles = require('./staticFiles');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -17,6 +18,9 @@ var port = 3000;
 var config = JSON.parse(fs.readFileSync('config.json'));
 var stylesheet = fs.readFileSync('public/gallery.css');
 var script = fs.readFileSync('public/gallery.js');
+
+/* load public directory */
+staticFiles.loadDir('public');
 
 /* load tempaltes */
 template.loadDir('templates');
@@ -159,16 +163,20 @@ function handleRequest(req, res) {
         uploadImage(req, res);
       }
       break;
-    case '/gallery.css':
-      res.setHeader('Content-Type', 'text/css');
-      res.end(stylesheet);
-      break;
-    case '/gallery.js':
-      res.setHeader('Content-Type', 'text/javascript');
-      res.end(script);
-      break;
+    // case '/gallery.css':
+    //   res.setHeader('Content-Type', 'text/css');
+    //   res.end(stylesheet);
+    //   break;
+    // case '/gallery.js':
+    //   res.setHeader('Content-Type', 'text/javascript');
+    //   res.end(script);
+    //   break;
     default:
-      serveImage(req.url, req, res);
+      if(staticFiles.isCached('public' + req.url)) {
+        staticFiles.serveFile('public' + req.url, req, res);
+      } else {
+        serveImage(req.url, req, res);
+      }
   }
 }
 
